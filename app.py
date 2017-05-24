c646a2570e555b429a27ad4880bdc5f6cc2d2020
rsreddy <radhika.b.shah@gmail.com>
2017-05-23 23:01:56 -0500

    added refresh and fixed up Jinja syntax

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ def index():
     return render_template(
         'index.html')
 
-@app.route('/question')
-def question():
+@app.route('/question/<int:question_id>')
+def question(question_id):
+    
     return render_template(
-        'questions/1.html')
-        
+        'questions/'+str(question_id)+'.html')
+     
 @app.route('/error')
 def error():
     return render_template(
@@ -24,35 +25,57 @@ def success():
     return render_template(
         'success.html')
 
-@app.route('/about')
+@app.route('/about/<int:score>')
 def about():
     return render_template(
         'about.html')
-        
+
 @app.route('/questions/<string:answer>/<int:question_id>')
 def questions( answer, question_id ):
     try:
+        score = 0
         if question_id == 1:
-            if answer == "blue":
+            question_id += 1
+            if answer == "15th":
+                score += 1
                 return render_template(
-                'success.html')
+                    'success.html',
+                    next=question_id,
+                    score=score)
             else:
                 return render_template(
                     'error.html',
-                    correctAnswer = "blue")
+                    correctAnswer="15th Amendment",
+                    next=question_id)
         elif question_id == 2:
+            question_id += 1
             if answer == "YOUR ANSWER HERE":
+                score += 1
                 return render_template(
-                    'success.html')
+                    'success.html',
+                    next=question_id,
+                    score=score)
             else:
                 return render_template(
                     'error.html',
-                    correctAnswer = "YOUR CORRECT ANSWERE HERE")
+                    correctAnswer="YOUR CORRECT ANSWERE HERE",
+                    next=question_id)
+        elif question_id == 5:
+            if answer == "YOUR ANSWER HERE":
+                score += 1
+                return render_template(
+                    'success.html',
+                    score=score,
+                    finished=True)
+            else:
+                return render_template(
+                    'error.html',
+                    correctAnswer="YOUR CORRECT ANSWERE HERE",
+                    finished=True)
 
         #TODO
         # Add 3more for each question id and fix your template accordingly
     except ValueError:
-        print ('Oops thats an error should be caught')
-    
+        print 'Oops thats an error should be caught'
 
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
